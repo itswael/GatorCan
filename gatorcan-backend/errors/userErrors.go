7bedb9a7cfeec70378e8067491923eb16d258d1b
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-03-14 13:29:55 -0400

    #issue232: Implemented single point error directories

---
+++ gatorcan-backend/errors/userErrors.go
@@ -0,0 +1,9 @@
+package errors
+
+import "errors"
+
+var (
+	ErrUserNotFound    = errors.New("user not found")
+	ErrUserExists      = errors.New("user already exists")
+	ErrInvalidPassword = errors.New("invalid password")
+)
