b77de99bd87938b64af4a248d7cadb95c71eee99
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-03-23 13:07:07 -0400

    #issue188: implemented assignments repository

---
+++ gatorcan-backend/repositories/assignmentRepository.go
@@ -0,0 +1,30 @@
+package repositories
+
+import (
+	"gatorcan-backend/models"
+
+	"gorm.io/gorm"
+)
+
+type AssignmentRepository interface {
+	GetAssignmentsByCourseID(courseID int) ([]models.Assignment, error)
+	GetAssignmentByIDAndCourseID(assignmentID int) (models.Assignment, error)
+}
+
+type assignmentRepository struct {
+	db *gorm.DB
+}
+
+// GetAssignmentByIDAndCourseID implements AssignmentRepository.
+func (a *assignmentRepository) GetAssignmentByIDAndCourseID(assignmentID int) (models.Assignment, error) {
+	panic("unimplemented")
+}
+
+// GetAssignmentsByCourseID implements AssignmentRepository.
+func (a *assignmentRepository) GetAssignmentsByCourseID(courseID int) ([]models.Assignment, error) {
+	panic("unimplemented")
+}
+
+func NewAssignmentRepository(db *gorm.DB) AssignmentRepository {
+	return &assignmentRepository{db: db}
+}
