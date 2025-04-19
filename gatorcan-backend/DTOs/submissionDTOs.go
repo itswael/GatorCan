b9e7ca462efe3d2ee85978fc1ae3a3d7f61f558e
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-04-19 14:19:44 -0400

    #issue341: added dto for grade response

--- gatorcan-backend/DTOs/submissionDTOs.go
+++ gatorcan-backend/DTOs/submissionDTOs.go
@@ -1,5 +1,7 @@
 package dtos
 
+import "time"
+
 // SubmissionResponseDTO represents the response structure for a submission.
 type SubmissionResponseDTO struct {
 	Grade       int    `json:"grade"`
@@ -17,6 +19,20 @@ type SubmissionRequestDTO struct {
 	FileURL      string `json:"file_url"`
 	FileName     string `json:"file_name"`
 	FileType     string `json:"file_type"`
+}
+
+type GradeResponseDTO struct {
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	Title       string    `gorm:"not null" json:"title"`
+	Grade       int       `json:"grade"`
+	MaxPoints   int       `json:"max_points"`
+	SubmittedAt time.Time `json:"date_of_submission"`
+	Feedback    string    `json:"feedback"`
+	Deadline    time.Time `json:"deadline"`
+	Mean        float64   `json:"mean"`
+	Median      float64   `json:"median"`
+	Highest     float64   `json:"highest"`
+	Lowest      float64   `json:"lowest"`
 }
 
 func NewSubmissionRequestDTO(assignmentID uint, fileURL, fileName, fileType string) *SubmissionRequestDTO {
