a7f32cfb206ec120acdf73d0d00230408a726b66
Harsh Gupta <harshhvg999@gmail.com>
2025-04-21 20:38:44 -0400

    aws-amplify configuration

--- gatorcan-frontend/src/main.jsx
+++ gatorcan-frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import "./amplify-config";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
